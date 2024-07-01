071de1100f2fd7aaa7385d1dddc8c7b358d085a3
AlliyaB <buchanana@stu.otc.school.nz>
2024-07-02 10:14:21 +1200

    Sign in validated, log in almost validated

--- Tertiary_budget_tracker.py
+++ Tertiary_budget_tracker.py
@@ -36,13 +36,19 @@ def open_signup_Window():
             except FileNotFoundError:
                 usernames = []
             if not username.isalpha() and not username.isnumeric():
-                messagebox.showerror("Invalid username", "Please only enter numbers or letters for username.")
+                messagebox.showerror("Invalid username", "Please only " +
+                                     "enter numbers or letters for username.")
             elif username in usernames:
-                messagebox.showerror("Invalid username", "This username already exists.\nPlease enter a different username.")
+                messagebox.showerror("Invalid username", "This username " +
+                                     "already exists.\nPlease enter a " +
+                                     "different username.")
             elif not first_name.isalpha() or not last_name.isalpha():
-                messagebox.showerror("Invalid first name/last name", "Please only enter letters for first name and last name.")
+                messagebox.showerror("Invalid first name/last " +
+                                     "name", "Please only enter letters " +
+                                     "for first name and last name.")
             elif password != confirm_password:
-                messagebox.showerror("Invalid password", "Please check that your passwords match.")
+                messagebox.showerror("Invalid password", "Please check " +
+                                     "that your passwords match.")
             else:
                 with open("Existing Users.txt", "a") as file:
                     file.write(f"{username}\n")
@@ -59,7 +65,8 @@ def open_signup_Window():
                 confirm_password_var.set("")
                 messagebox.showinfo("Successful", "Signed up successfully")
         else:
-            messagebox.showerror("Invalid input", "There are missing fields.\nPlease enter all fields.")
+            messagebox.showerror("Invalid input", "There are missing " +
+                                 "fields.\nPlease enter all fields.")
 
     # Create window properties.
     signup_window = Toplevel(first_window)
@@ -68,49 +75,70 @@ def open_signup_Window():
     signup_window.resizable(False, False)
 
     # Declaring name and password as string variables.
-    first_name_var=tk.StringVar()
+    first_name_var = tk.StringVar()
     last_name_var = tk.StringVar()
     username_var = tk.StringVar()
     password_var = tk.StringVar()
     confirm_password_var = tk.StringVar()
     
     # Create window content with labels, canvas, and buttons.
-    canvas = Canvas(signup_window, height = 50, width = 350, bg = "CadetBlue2")
-    canvas.place(x = 0, y = 20)
-    title_lbl = Label(signup_window, text = "Sign up", font = ("Helvetica", 15))
+    canvas = Canvas(signup_window, 
+                    height = 50, 
+                    width = 350, 
+                    bg = "CadetBlue2")
+    title_lbl = Label(signup_window, 
+                      text = "Sign up", 
+                      font = ("Helvetica", 15))
     subtitle_lbl = Label(signup_window, 
-                            text = "Create an account",
-                            font = ("Helvetica", 15))
+                        text = "Create an account",
+                        font = ("Helvetica", 15))
     login_btn = tk.Button(signup_window,
-                    text = "Log in",
-                    width = 10,
-                    height = 2,
-                    fg = "black",
-                    bg = "darkgrey",
-                    command = open_login_Window
-            )
+                          text = "Log in",
+                          width = 10,
+                          height = 2,
+                          fg = "black",
+                          bg = "darkgrey",
+                          command = open_login_Window)
             
-    first_name_lbl = tk.Label(signup_window, text = "First name:", font=("Helvetica", 10, "bold"))
-    last_name_lbl = tk.Label(signup_window, text = "Last name:", font=("Helvetica", 10, "bold"))
-    username_lbl = tk.Label(signup_window, text = "Username:", font=("Helvetica", 10, "bold"))
-    password_lbl = tk.Label(signup_window, text = "Password:", font = ("Helvetica", 10, "bold"))
-    confirm_password_lbl = tk.Label(signup_window, text = "Confirm \nPassword:", font = ("Helvetica", 10, "bold"))
+    first_name_lbl = tk.Label(signup_window, 
+                              text = "First name:", 
+                              font = ("Helvetica", 10, "bold"))
+    last_name_lbl = tk.Label(signup_window, 
+                             text = "Last name:", 
+                             font = ("Helvetica", 10, "bold"))
+    username_lbl = tk.Label(signup_window, 
+                            text = "Username:", 
+                            font = ("Helvetica", 10, "bold"))
+    password_lbl = tk.Label(signup_window, 
+                            text = "Password:", 
+                            font = ("Helvetica", 10, "bold"))
+    confirm_password_lbl = tk.Label(signup_window, 
+                                    text = "Confirm \nPassword:", 
+                                    font = ("Helvetica", 10, "bold"))
     signup_btn = tk.Button(signup_window,
-                    text = "Sign up", 
-                    width = 7,
-                    height = 1,
-                    fg = "black",
-                    bg = "gold",
-                    command = signup)
+                           text = "Sign up", 
+                           width = 7,
+                           height = 1,
+                           fg = "black",
+                           bg = "gold",
+                           command = signup)
 
     # Entrys and validation.
-    first_name_entry = tk.Entry(signup_window, textvariable = first_name_var, font=("Helvetica", 10))
-    last_name_entry = tk.Entry(signup_window, textvariable = last_name_var, font=("Helvetica", 10))
-    username_entry = tk.Entry(signup_window, textvariable = username_var, font=("Helvetica", 10))
-    password_entry=tk.Entry(signup_window, textvariable = password_var, font = ("Helvetica", 10), show = "*")
-    confirm_password_entry=tk.Entry(signup_window, textvariable = confirm_password_var, font = ("Helvetica", 10), show = "*")
+    first_name_entry = tk.Entry(signup_window, 
+                                textvariable = first_name_var)
+    last_name_entry = tk.Entry(signup_window, 
+                               textvariable = last_name_var)
+    username_entry = tk.Entry(signup_window, 
+                              textvariable = username_var)
+    password_entry=tk.Entry(signup_window, 
+                            textvariable = password_var, 
+                            show = "*")
+    confirm_password_entry=tk.Entry(signup_window, 
+                                    textvariable = confirm_password_var, 
+                                    show = "*")
 
     # Placing the labels and entries.
+    canvas.place(x = 0, y = 20)
     login_btn.place(x = 200, y = 27)
     title_lbl.place(x = 10, y = 36)
     subtitle_lbl.place(x = 70, y = 80)
@@ -140,16 +168,25 @@ def open_login_Window():
         username = username_var.get()
         password = password_var.get()
 
-        # Open and read the existing users file.
-        storing_name = open("Existing Users.txt", "a")
-        storing_name.read()
-        # Validate user input.
-        if username and password in storing_name:
-            messagebox.showinfo("Successful", "Log in successfully.")
-            username_var.set("")
-            password_var.set("")
-        # elif first_name.isalpha() and last_name.isalpha() and password == confirm_password:
-        storing_name.close()
+        if username and password:
+            try:
+                with open("Existing Users.txt", "r") as file:
+                    lines = file.read().splitlines()
+                    # Check if a line has username and password.
+                    user_exists = any(f"Username: {username}" in line and f"Password: {password}" in line for line in lines)
+                if user_exists:
+                    print(f"Username: {username}")
+                    print(f"Password: {password}")
+                    username_var.set("")
+                    password_var.set("")
+                    messagebox.showinfo("Successful", "Log in successful.")
+            except FileNotFoundError:
+                messagebox.showerror("Unsuccessful", "You have entered " +
+                                     "an invalid username or password. " +
+                                     "Please try again or sign up.")
+        else:
+            messagebox.showerror("Invalid input", "There are missing " +
+                                 "fields.\nPlease enter all fields.")
 
     # Create window properties.
     login_window = Toplevel(first_window)
@@ -162,36 +199,48 @@ def open_login_Window():
     password_var = tk.StringVar()
 
     # Create window content with labels, canvas, and buttons.
-    canvas = Canvas(login_window, height = 50, width = 350, bg = "CadetBlue2", )
-    canvas.place(x = 0, y = 20)
-    title_lbl = Label(login_window, text = "Log in", font = ("Helvetica", 15))
+    canvas = Canvas(login_window, 
+                    height = 50, 
+                    width = 350, 
+                    bg = "CadetBlue2", 
+    )
+    title_lbl = Label(login_window, 
+                      text = "Log in", 
+                      font = ("Helvetica", 15))
     subtitle_lbl = Label(login_window, 
-                            text = "Log in to your account",
-                            font = ("Helvetica", 15))
+                         text = "Log in to your account",
+                         font = ("Helvetica", 15))
     open_signup_btn = tk.Button(login_window,
-                    text = "Sign up",
-                    width = 10,
-                    height = 2,
-                    fg = "black",
-                    bg = "darkgrey",
-                    command = open_signup_Window
-            )
+                                text = "Sign up",
+                                width = 10,
+                                height = 2,
+                                fg = "black",
+                                bg = "darkgrey",
+                                command = open_signup_Window)
 
-    username_lbl = tk.Label(login_window, text = "Username:", font=("Helvetica", 10, "bold"))
-    password_lbl = tk.Label(login_window, text = "Password:", font = ("Helvetica", 10, "bold"))
+    username_lbl = tk.Label(login_window, 
+                            text = "Username:", 
+                            font=("Helvetica", 10, "bold"))
+    password_lbl = tk.Label(login_window, 
+                            text = "Password:", 
+                            font = ("Helvetica", 10, "bold"))
     login_btn = tk.Button(login_window,
-                    text = "Log in", 
-                    width = 7,
-                    height = 1,
-                    fg = "black",
-                    bg = "gold",
-                    command = login)
+                          text = "Log in", 
+                          width = 7,
+                          height = 1,
+                          fg = "black",
+                          bg = "gold",
+                          command = login)
 
     # Create entries
-    username_entry = tk.Entry(login_window)
-    password_entry = tk.Entry(login_window, show = "*")
+    username_entry = tk.Entry(login_window, 
+                              textvariable = username_var)
+    password_entry = tk.Entry(login_window, 
+                              textvariable = password_var, 
+                              show = "*")
 
     # Place the labels and entries.
+    canvas.place(x = 0, y = 20)
     open_signup_btn.place(x = 200, y = 27)
     title_lbl.place(x = 10, y = 36)
     subtitle_lbl.place(x = 60, y = 80)
@@ -256,7 +305,9 @@ info_lbl_one = Label(first_window, text = "Enjoy a budget plan tailored " +
                  "\nto your financial wants and needs.\n\nBenefit from " +
                  "helpful tips to\nmanage your money.", 
                  font = ("Helvetica", 14))
-info_lbl_two = Label(first_window, text = "Aims to improve financial stability among New Zealand tertiary students aged 18-25.", font = ("Helvetica", 10))
+info_lbl_two = Label(first_window, text = "Aims to improve financial " +
+                     "stability among New Zealand tertiary students " +
+                     "aged 18-25.", font = ("Helvetica", 10))
 info_lbl_one.place(x = 800, y = 280)
 info_lbl_two.place(x = 200, y = 400)
 
@@ -275,4 +326,18 @@ label1.place(x = 0, y = 450)
 
 first_window.mainloop()
 
-first_window.destroy()
+# Create building profile page, user will be directed here after signing up.
+if first_window.destroy():
+    building_profile_window = tk.Tk()
+    building_profile_window.geometry("1200x750")
+    building_profile_window.title("Building my profile")
+    building_profile_window.resizable(False, False)
+
+    # Create a coloured box for the top where navigation bar will be.
+    canvas = Canvas(first_window, 
+                    height = 100, 
+                    width = 1210, 
+                    bg = "CadetBlue2", )
+    canvas.place(x = 0, y = 20)
+
+    building_profile_window.mainloop()
